c9482da1e669676cb9910276c1f8a0fc857c9914
Quarris <tomasz.winnicki@live.co.uk>
2023-08-24 02:27:17 +0100

    Fixed curio deleting backpacks

--- src/main/java/dev/quarris/enigmaticgraves/EnigmaticGraves.java
+++ src/main/java/dev/quarris/enigmaticgraves/EnigmaticGraves.java
@@ -4,6 +4,8 @@ import dev.quarris.enigmaticgraves.config.GraveConfigs;
 import dev.quarris.enigmaticgraves.grave.GraveManager;
 import dev.quarris.enigmaticgraves.setup.Registry;
 import dev.quarris.enigmaticgraves.utils.ModRef;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
